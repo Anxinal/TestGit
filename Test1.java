6536f6042afb7af7be9bd82477935eb76c6ccab1
Anxinal <ax1375240926@gmail.com>
2025-05-13 18:10:33 +0800

    Update on the planner

--- Test1.java
+++ Test1.java
@@ -3,4 +3,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Let's make a plan");
     }
+
+    public static void planner1(String planString){
+        System.out.println("This is going to be my plan: "+ planString);
+    }
 } 
